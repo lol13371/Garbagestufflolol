6f31da75481c8497265f877135f18dc02abdb627
lol13371 <117176678+lol13371@users.noreply.github.com>
2023-02-16 03:40:39 +0600

    Update main.b607e494.chunk.js

--- static/js/main.b607e494.chunk.js
+++ static/js/main.b607e494.chunk.js
@@ -659,7 +659,7 @@
                                             })]
                                         }), Object(N.jsxs)("h2", {
                                             className: "mint-live text-shadow",
-                                            children: ["ENDS 16TH FEB"
+                                            children: ["ENDS IN ", Object(N.jsx)("span", {
                                                 className: "text-primary",
                                                 id: "demo",
                                                 children: ""
